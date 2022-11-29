c50d2e5af54de8a342316fc9b762ad12486befd8
sondes rajhi <100100547+linarajhi2023@users.noreply.github.com>
2022-11-29 16:20:06 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+import React from 'react';
+import HomeSection from './Components/HomeSection/HomeSection';
+
+
+function App() {
+  
+  
+  return (
+     <>
+   
+
+
+     <HomeSection />
+
+     </>
+  );
+}
+
+export default App;
